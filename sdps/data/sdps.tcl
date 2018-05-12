c7d99070ff8de2d3eb6e7f4e63e56c937c0ac97b
Yisong Chang <changyisong@ict.ac.cn>
2018-05-12 20:34:06 +0800

    sdhci: Add support for using root file system in SD card

--- sdps/data/sdps.tcl
+++ sdps/data/sdps.tcl
@@ -24,6 +24,10 @@ proc generate {drv_handle} {
     set clk_freq [hsi::utils::get_ip_param_value $ip C_SDIO_CLK_FREQ_HZ]
     set_property CONFIG.clock-frequency "$clk_freq" $drv_handle
     set_drv_conf_prop $drv_handle C_MIO_BANK xlnx,mio_bank hexint
+    set_drv_prop $drv_handle "broken-mmc-highspeed" "" boolean
+    set_drv_prop $drv_handle "disable-wp" "" boolean
+    set_drv_prop $drv_handle "no-1-8-v" "" boolean
+    set_property CONFIG.max-frequency "40000000" $drv_handle
 }
 
 
